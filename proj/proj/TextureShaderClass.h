7c7d2a5946f613ade550f6eafec0cfba502375d9
jeong <daddygp94@gmail.com>
2020-05-01 23:09:51 +0900

    Add TextureShaderClass

---
+++ proj/proj/TextureShaderClass.h
@@ -0,0 +1,47 @@
+#pragma once
+
+class TextureShaderClass : public AlignedAllocationPolicy<16>
+{
+private:
+	struct ConstantBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
+
+	struct PixelBufferType
+	{
+		XMFLOAT4 pixelColor;
+	};
+
+public:
+	TextureShaderClass();
+	TextureShaderClass(const TextureShaderClass&);
+	~TextureShaderClass();
+
+	bool FontInitialize(ID3D11Device*, HWND);
+	bool TextureInitialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool FontRender(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4);
+	bool TextureRender(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*);
+
+private:
+	bool FontInitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	bool TextureInitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4);
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*);
+	void RenderShader(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader* m_vertexShader = nullptr;
+	ID3D11PixelShader* m_pixelShader = nullptr;
+	ID3D11InputLayout* m_layout = nullptr;
+	ID3D11Buffer* m_constantBuffer = nullptr;
+	ID3D11SamplerState* m_sampleState = nullptr;
+	ID3D11Buffer* m_pixelBuffer = nullptr;
+};
+
